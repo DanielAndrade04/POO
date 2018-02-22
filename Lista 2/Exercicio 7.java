4d7b25778ebeddcbd70ef60bfb1739d3cb46defa
Daniel Gonçalves <31700067+DanielAndrade04@users.noreply.github.com>
2018-02-22 20:05:03 -0300

    Create Exercicio 7.java

---
+++ Lista 2/Exercicio 7.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.*;
+import java.util.*;
+public class main {
+
+	public static void main (String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		Double n1 = teclado.nextDouble();
+		Double n2 = teclado.nextDouble();
+		Double n3 = teclado.nextDouble();
+		Double calc = n1 + n2 + n3;
+		if (calc >= 60)
+		System.out.print ( "Aprovado");
+		else
+		System.out.print ( "Reprovado");
+	
+	}
+}
